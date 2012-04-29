821ec9a190dcddd71978f91a17bdbaabf91b4f8b
Alexei Gousev <numegil@gmail.com>
2012-04-29 09:35:47 -0700

    Fixed a bunch of bugs

--- Readme.txt
+++ Readme.txt
@@ -3,6 +3,6 @@ This simple app allows you to view outstanding tables in DCIR on your phone whil
 There are three components:
 1) Python script named 'monitor.py' which must run continuously on the computer running DCIR.  You need to set the FILE_DIRECTORY and FILE_PREFIX variables in the script to match the tournament you're running.
 2) The webserver backend.  This is in the 'dcif_tables' directory of the repository.  I have the code deployed to Heroku, so you probably don't need to worry about this part.
-3) The client.  Currently, only an iPhone version of the client exists.  I hope to add an Android version soon.  Additionally, you can visit the web server att 'http://cold-galaxy-7337.herokuapp.com/get/' to view the current outstandingg tables.
+3) The client.  There exist iPhone and Android versions of the client app.  The Android version can be installed directly via the APK file in this repository.  Installing the iPhone version is significantly less trivial.  Additionally, you can visit the web server at 'http://cold-galaxy-7337.herokuapp.com/get/' to view the current outstanding tables.
 
-This is all a very hastily put together rough draft of the app.  There's a bajillion features missing;  I want to try it first to see if it's worth more of my time.
+This is all a very hastily put together rough draft of the app.  There's a bajillion features missing.  I want to try it first to see if it's worth more of my time.
--- monitor.py
+++ monitor.py
@@ -2,11 +2,15 @@ import os, pdb, urllib2
 from time import sleep
 from datetime import datetime
 
-FILE_DIRECTORY = "C:\\Program Files\\Wizards of the Coast\\Version3"
-FILE_PREFIX = "OKRW"
+# Change these to match your current tournament
+FILE_DIRECTORY = "/Users/numegil/Downloads"
+FILE_PREFIX = "WLOC"
+STARTING_TABLE = 1
+
+# Don't change this one unless you know what you're doing
 WEB_URL = "http://cold-galaxy-7337.herokuapp.com/set/"
 
-path = FILE_DIRECTORY + '\\' + FILE_PREFIX + '303.dat'
+path = FILE_DIRECTORY + '/' + FILE_PREFIX + '303.dat'
 
 m_time = 0
 while(True):
@@ -15,15 +19,39 @@ while(True):
     if m_time != os.path.getmtime(path):
         m_time = os.path.getmtime(path)
         
-        # Get every third line of the file
-        lines = open(path).read().split('\n')[2::3]
+        # Open the file
+        lines = open(path).read().split('\n')
+        
+        # Figure out what the current round is
+        cur_round = 0
+        for line in lines:
+            if len(line) < 1:
+                continue
+            
+            # First digit of each line is the round (I think...)
+            if int(line[0]) < cur_round:
+                break
+            else:
+                cur_round = int(line[0])
+        
+        # We only care about the most recent round
+        lines = lines[cur_round-1::cur_round]
         
         # Figure out which tables don't have results yet
         outstanding = []
         for i, line in enumerate(lines):
+            if len(line) < 1:
+                continue
+            
             chars = line.split(',')
+               
+            # If the second digit is a 0, this table is invalid and ignore it
+            if chars[1] == '0':
+                continue 
+            
+            # If there's no match result, append it to outstanding tables
             if chars[4] == '0' and chars[5] == '0':
-                outstanding.append(str(i+1))
+                outstanding.append(str(i+STARTING_TABLE))
         
         # Construct our GET parameter
         param = ','.join(outstanding)
